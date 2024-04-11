8e9ba5e755793047a68af83246f7307189bb780f
Parameswari Bala <parameswaribala@gmail.com>
2024-04-11 18:03:40 +0530

    updated

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/9/b09c6944fef7001e1202e2acbb2d01f3
@@ -0,0 +1,13 @@
+package com.tg.healthcareapp.models;
+
+import com.tg.healthcareapp.facades.DataSource;
+
+public class MysqlDataSource implements DataSource{
+
+	@Override
+	public void addData(String message) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- healthcareapp/src/main/java/com/tg/healthcareapp/models/MysqlDataSource.java
+++ healthcareapp/src/main/java/com/tg/healthcareapp/models/MysqlDataSource.java
@@ -1,13 +1,21 @@
 package com.tg.healthcareapp.models;
 
-import com.tg.healthcareapp.facades.DataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
 
+import com.tg.healthcareapp.facades.DataSource;
+@Component
+@Primary
 public class MysqlDataSource implements DataSource{
 
+	private static final Logger logger= LoggerFactory.
+			getLogger(MysqlDataSource.class);
 	@Override
 	public void addData(String message) {
 		// TODO Auto-generated method stub
-		
+		logger.info("Mysql"+message);
 	}
 
 }

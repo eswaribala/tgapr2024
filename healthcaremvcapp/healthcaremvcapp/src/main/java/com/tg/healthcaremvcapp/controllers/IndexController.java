ac80c48d8b9033ef01fdcc26b6f4354d4aee56dc
Parameswari Bala <parameswaribala@gmail.com>
2024-04-12 17:31:03 +0530

    updated

--- healthcaremvcapp/healthcaremvcapp/src/main/java/com/tg/healthcaremvcapp/controllers/IndexController.java
+++ healthcaremvcapp/healthcaremvcapp/src/main/java/com/tg/healthcaremvcapp/controllers/IndexController.java
@@ -1,14 +1,32 @@
 package com.tg.healthcaremvcapp.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.tg.healthcaremvcapp.models.Clinic;
 
 @Controller
 public class IndexController {
 	
 	@GetMapping("/index")
-	public String loadIndex() {
+	public String loadIndex(Model model) {
+		
+		model.addAttribute("clinic", new Clinic());
 		return "index.html";
+	}
+	
+	@PostMapping("/register")
+	public String saveClinicData(@ModelAttribute("clinic")Clinic clinic, 
+			Model model) {
+		
+		if(clinic.getClinicId()!=null)
+			return "success.html";
+		else
+			return "redirect:/index";	
+		
 	}
 
 }
